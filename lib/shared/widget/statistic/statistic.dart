5ec4b037e9e836191d0e0bcb174d97b5fb4b5d64
denyocr <denyocr.world@gmail.com>
2024-01-30 12:17:49 +0700

    .

---
+++ lib/shared/widget/statistic/statistic.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:hyper_ui/core.dart';
+
+class QStatistic extends StatelessWidget {
+  final String label;
+  final num value;
+  final IconData icon;
+  final Color? color;
+  const QStatistic({
+    Key? key,
+    required this.label,
+    required this.value,
+    required this.icon,
+    this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.all(12.0),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "$label",
+                    style: TextStyle(
+                      fontSize: 12.0,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 6.0,
+                  ),
+                  Text(
+                    "${value}",
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(6.0),
+              decoration: BoxDecoration(
+                color: color ?? primaryColor,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(
+                    8.0,
+                  ),
+                ),
+              ),
+              child: Icon(
+                icon,
+                size: 16.0,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
